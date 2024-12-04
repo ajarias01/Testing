ac8fe376f59b4ecc502c7cb0ffaeb2fb58d652bd
Alan Joel Arias Iza <ariasalan634@gmail.com>
2024-12-03 23:55:52 -0500

    FileJsonManage

---
+++ Galaxy/src/ec/edu/espe/galaxy/view/GalaxyApp.java
@@ -0,0 +1,12 @@
+
+
+package ec.edu.espe.galaxy.view;
+/**
+ *
+ * @author Alan
+ */
+public class GalaxyApp {
+    public static void main(String[] args) {
+        MenuOptions.showMenuOptions();
+    }
+}
